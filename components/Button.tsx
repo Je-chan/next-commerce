d40df5924aeefacd9ec1fa658d7dfac852e0a312
ye chan <81739782+Je-chan@users.noreply.github.com>
2023-05-25 22:06:38 +0900

    Update Button.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,9 +1,5 @@
 import styled from '@emotion/styled'
 
-// export default function Button() {
-//   return <div>Button</div>
-// }
-
 const Button = styled.button`
   padding: 1rem;
   border-radius: 0.5rem;
